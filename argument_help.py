ecc1481953f6cee24dbbc49650c454dd26d73efd
Vatsal Veerwal <vatsalveerwal@gmail.com>
2020-10-31 19:14:09 +0530

    Cleaning the code.

---
+++ argument_help.py
@@ -0,0 +1,39 @@
+import getopt
+import sys
+
+
+def get_options(argv):
+	try:
+		options, arguments = getopt.getopt(sys.argv[1:], "s:p:c:h", ["subreddit=", "post_limit=", "pl=", "comment_limit=", "cl=", "help"])
+	except getopt.GetoptError:
+		input_help(2)
+	return options
+
+def input_help(exit_code=0):
+	"""
+	Provides help with command line arguments and exits the process with the given exit code.
+	"""
+	print("reddit_bot.py -s <subreddit expression> -p <post limit> -c <comment_limit>")
+	print("reddit_bot.py -s <subreddit expression> --pl <post limit> --cl <comment_limit>")
+	print("reddit_bot.py --subreddit <subreddit expression> --post_limit <post limit> --comment_limit <comment limit>")
+	sys.exit(exit_code)
+
+def get_args(options):
+	"""
+	Takes the options list and returns the chosen `subreddit`, `post_limit`, and `comment_limit`. If some options are not provided, returns the default values for them. If a different option is provided, invokes the `input_help` function.
+	"""
+	subreddit = "AskReddit"
+	post_limit = 10
+	comment_limit = 3
+	for option, argument in options:
+		if option in ("-h", "--help"):
+			input_help()
+		elif option in ("-s", "--subreddit"):
+			subreddit = argument
+		elif option in ("-p", "--pl", "--post_limit"):
+			post_limit = int(argument)
+		elif option in ("-c", "--cl", "--comment_limit"):
+			comment_limit = int(argument)
+		else:
+			input_help(1)
+	return subreddit, post_limit, comment_limit
--- reddit_bot.py
+++ reddit_bot.py
@@ -1,44 +1,16 @@
-import sys
 import getopt
+import sys
+
 import praw
+
+from argument_help import get_args, get_options
 from authenticate import authenticate
 from screenshot_capture import Capturer
 
-def input_help(exit_code=0):
-	"""
-	Provides help with command line arguments and exits the process with the given exit code.
-	"""
-	print("reddit_bot.py -s <subreddit expression> -p <post limit> -c <last_comment_number>")
-	print("reddit_bot.py -s <subreddit expression> --pl <post limit> --cl <last_comment_number>")
-	print("reddit_bot.py --subreddit <subreddit expression> --post_limit <post limit> --last_comment_number <comment limit>")
-	sys.exit(exit_code)
-
-def get_args(options):
-	"""
-	Takes the options list and returns the chosen `subreddit`, `post_limit`, and `last_comment_number`. If some options are not provided, returns the default values for them. If a different option is provided, invokes the `input_help` function.
-	"""
-	subreddit = "AskReddit"
-	post_limit = 10
-	last_comment_number = 3
-	for option, argument in options:
-		if option in ("-h", "--help"):
-			input_help()
-		elif option in ("-s", "--subreddit"):
-			subreddit = argument
-		elif option in ("-p", "--pl", "--post_limit"):
-			post_limit = int(argument)
-		elif option in ("-c", "--lcn", "--last_comment_number"):
-			last_comment_number = int(argument)
-		else:
-			input_help(1)
-	return subreddit, post_limit, last_comment_number
 
 def main():
-	try:
-		options, arguments = getopt.getopt(sys.argv[1:], "s:p:c:h", ["subreddit=", "post_limit=", "pl=", "last_comment_number=", "cl=", "help"])
-	except getopt.GetoptError:
-		input_help(2)
-	subreddit, post_limit, last_comment_number = get_args(options)
+	options = get_options(sys.argv[1:])
+	subreddit, post_limit, comment_limit = get_args(options)
 	post_number = 1
 	cpt = Capturer()
 	reddit = authenticate()
@@ -63,11 +35,11 @@ def main():
 				cpt.create_screenshot(comment.permalink, post_number, is_comment=True, comment_number=comment_number)
 				cpt.create_speech(f"{comment.author} said: {comment.body}", post_number, is_comment=True, comment_number=comment_number)
 				cpt.create_videoclip(post_number,is_comment=True,  comment_number=comment_number)
-				if comment_number == last_comment_number:
+				if comment_number == comment_limit:
 					break
 				comment_number +=  1
 			post_number += 1
-	cpt.create_final_video(post_number, last_comment_number)
+	cpt.create_final_video(post_number, comment_limit)
 
 
 
